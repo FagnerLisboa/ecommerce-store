1ad73fb4975fa5fd157a377cf33c8bd51f20e3f9
Fagner Lisboa <fnlisboa2016@gmail.com>
2025-05-22 18:07:29 -0300

    Removing Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSidenavModule } from '@angular/material/sidenav';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -38,7 +37,6 @@ import { AdministratorComponent } from './administrator/administrator.component'
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
